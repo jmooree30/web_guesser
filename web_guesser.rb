7e8168ed4d32c441a077c35fabd11220356d52be
Jacob <jmooree30@gmail.com>
2017-10-11 00:47:22 -0400

    Added color change with guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(100)
+color_var = '#0000FF'
+
+def switch_c(guess,number)
+  if guess.to_i > number + 5
+    color_var = '#0000FF'
+  elsif guess.to_i < number - 5
+    color_var = '#0000FF'
+  elsif guess.to_i > number 
+    color_var = '#00FFFF'
+  elsif guess.to_i < number
+    color_var = '#00FFFF' 
+  else guess.to_i == number 
+    color_var = "green"
+  end
+end 
 
 def high(guess,number)
   if guess.to_i > number + 5
@@ -20,5 +35,7 @@ end
 get '/' do
  guess = params["guess"]
  check_guess = high(guess.to_i,number)
- erb :index, :locals => {:number => number, :guess => guess, :check_guess => check_guess}
+ color = switch_c(guess,number)
+ erb :index, :locals => {:number => number, :guess => guess, :check_guess => check_guess, :color_var => color_var, :color => color}
 end
+
