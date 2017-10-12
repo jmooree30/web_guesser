b5f1271065774d14d3b33da5a587ded9a14a656b
Jacob <jmooree30@gmail.com>
2017-10-11 23:50:48 -0400

    Project complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,6 +5,7 @@ color_var = '#0000FF'
 @@number = rand(100)
 @@counter = 5
 @@message = "Please try again."
+@@cheater = ""
 
 def switch_c(guess,number)
   if guess.to_i > number + 5
@@ -38,16 +39,28 @@ def reset(message,number,counter)
   if @@counter == 0
     @@number = rand(100)
     @@counter = 5
-    @@message = "Please try again"
+    @@message
   end
+end 
+
+def correct_answer(number,guess)
+  if guess == number 
+    @@number = rand(100)
+    @@counter = 5
+  end 
 end 
 
 get '/' do
  @@counter -=1 
  guess = params["guess"]
+ cheat = params['cheat']
  check_guess = high(guess.to_i,@@number)
  color = switch_c(guess,@@number)
  try_again = reset(@@message,@@number,@@counter)
+ correct_answer(@@number,guess.to_i)
+ if cheat == 'true'
+   @@cheater = @@number
+ end
  erb :index, :locals => {:guess => guess, :check_guess => check_guess, :color_var => color_var, :color => color, :try_again => try_again}
 end
 
